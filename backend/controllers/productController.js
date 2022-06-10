740589295195e6cedeadb5837063201752e4b052
NguyenQuocTuan385 <81601059+NguyenQuocTuan385@users.noreply.github.com>
2022-06-10 20:29:00 +0700

    Get Single Product By It's ID

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,7 @@ exports.newProduct = async (req, res, next) => {
         product
     })
 }
+
 //Get all products => /api/v1/products
 exports.getProducts = async (req, res, next) => {
     const products = await Product.find()
@@ -15,5 +16,23 @@ exports.getProducts = async (req, res, next) => {
         success: true,
         count: products.length,
         products
+    })
+}
+
+//Get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
     })
 }
