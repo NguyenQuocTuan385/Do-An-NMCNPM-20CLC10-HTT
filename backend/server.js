fecd71c150e4b53e43e397b92709fa6cff373043
NguyenQuocTuan385 <81601059+NguyenQuocTuan385@users.noreply.github.com>
2022-06-11 11:16:06 +0700

    Cập nhật xử lý Uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require('./app')
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
 
+//Handle Uncaught exceptions.
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`)
+    console.log('Shutting down server due to uncaught exception')
+    process.exit(1)
+})
+
 //setting up config file
 dotenv.config({ path: 'backend/config/config.env' })
 
@@ -12,6 +19,7 @@ const server = app.listen(process.env.PORT || 4000, () => {
 })
 
 // Handle Unhandled Promise rejections
+//Xu Ly khi ket noi toi MongoDB bi yeu
 process.on('unhandledRejection', err => {
     console.log(`ERROR: ${err.message}`)
     console.log('Shutting down the server due to Unhandled Promise rejection')
