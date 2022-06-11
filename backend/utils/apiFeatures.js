2c83406f7ebc1710b61d32dbe7cb622d547309ae
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2022-06-11 22:37:53 +0700

    filter product

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -17,6 +17,27 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+    filter(){
+        const queryCopy = { ...this.querystr};
+        
+        console.log(queryCopy);
+        //Removing fields from the query
+        const removeFields = ['keyword','limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+       
+
+        console.log(queryCopy);
+
+        //advance filter for price,etc
+        let querystr = JSON.stringify(queryCopy)
+        querystr = querystr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        console.log(queryCopy);
+
+        this.query = this.query.find(JSON.parse(querystr));
+        return this;
+    }
+    
 }
 
 module.exports = APIFeatures
