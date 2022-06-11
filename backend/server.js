1dde23c77b62adff8cde67675facb6da92b13b71
NguyenQuocTuan385 <81601059+NguyenQuocTuan385@users.noreply.github.com>
2022-06-11 10:49:28 +0700

    Xử lý tắt Server khi kết nối tới Database bị lỗi

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ dotenv.config({ path: 'backend/config/config.env' })
 
 //Connecting to DB
 connectDatabase()
-app.listen(process.env.PORT || 4000, () => {
+const server = app.listen(process.env.PORT || 4000, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})
+
+// Handle Unhandled Promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`)
+    console.log('Shutting down the server due to Unhandled Promise rejection')
+    server.close(() => {
+        process.exit(1)
+    })
 })
