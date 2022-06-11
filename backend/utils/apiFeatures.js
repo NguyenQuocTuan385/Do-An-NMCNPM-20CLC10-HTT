9fc926f928b73b6fc20235094dee6352295b8adc
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2022-06-11 21:59:04 +0700

    search keyword

---
+++ backend/utils/apiFeatures.js
@@ -0,0 +1,22 @@
+class APIFeatures {
+    constructor(query,querystr){
+        this.query=query;
+        this.querystr=querystr;
+    }
+
+    search(){
+        const keyword = this.querystr.keyword ? {
+            name:{
+                $regex: this.querystr.keyword,
+                $options: 'i'
+            }
+        } : {}
+
+        console.log(keyword);
+        
+        this.query = this.query.find({ ...keyword });
+        return this;
+    }
+}
+
+module.exports = APIFeatures
