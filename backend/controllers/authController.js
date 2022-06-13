61b85b5a6ad02a8a6b63cbfecaf9665a86405ecc
nhunhatnguyen <nhatem0974@gmail.com>
2022-06-13 17:55:07 +0700

    Login User & Assign Token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middlewares/catchAsyncErrors');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 
 //Resgister a user
@@ -24,4 +25,31 @@ exports.registerUser = catchAsyncErrors(async(req, res, next) => {
         token
     })
 
+})
+
+exports.loginUser = catchAsyncErrors(async(req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email & password', 400))
+    }
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token
+    })
 })
