ba1e1ba55f8b4e9aa2800fff245157b503b25baa
NguyenQuocTuan385 <81601059+NguyenQuocTuan385@users.noreply.github.com>
2022-06-13 20:02:01 +0700

    Cập nhật không cho xem product nếu ko phải User

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const app = express()
 const errorMiddleWare = require('./middlewares/errors')
 const products = require('./routes/product')
 const auth = require('./routes/auth')
+const cookieParser = require('cookie-parser')
 
 app.use(express.json())
-    //Import all routes
+app.use(cookieParser())
 
+//Import all routes
 app.use('/api/v1', products)
 app.use('/api/v1', auth)
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-
+const { isAuthenticated } = require('../middlewares/auth')
 const { getProducts,
     newProduct,
     getSingleProduct,
     updateProduct,
     deleteProduct } = require('../controllers/productController')
 
-router.route('/products').get(getProducts)
+router.route('/products').get(isAuthenticated, getProducts)
 router.route('/admin/product/new').post(newProduct)
 router.route('/product/:id').get(getSingleProduct)
 router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct)
