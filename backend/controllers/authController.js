0ecf6f7a31c7582358dc879a5fd341a49a06e0a5
nhunhatnguyen <nhatem0974@gmail.com>
2022-06-13 17:22:35 +0700

    Generate JSON Web Token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,9 +16,12 @@ exports.registerUser = catchAsyncErrors(async(req, res, next) => {
             url: 'https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/272275516_1608903656118968_8706847657319824741_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=tbaEBY6rEucAX9PQfdr&_nc_ht=scontent.fsgn5-9.fna&oh=00_AT_JVtmbU6loyg3bNeK6c9SNPHsPXGDU2QGQ56wywKhu4A&oe=62AB5D25'
         }
     })
-})
 
-res.status(201).json({
-    success: true,
-    user
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+        success: true,
+        token
+    })
+
 })
