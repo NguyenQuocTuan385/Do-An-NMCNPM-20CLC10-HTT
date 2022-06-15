e585c509b65f1a102bb068355a55818fe143e152
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-06-15 23:03:09 +0700

    Design Header and Footer in Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 function App() {
   return (
     <div className="App">
-      <h1> ZoneBook</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
