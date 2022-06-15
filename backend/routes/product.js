e535124f49191b7c8d7d36b579149bf10ee7aa4e
NguyenQuocTuan385 <80704503+nhunhatnguyen@users.noreply.github.com>
2022-06-15 18:05:20 +0700

    Get All Reviews

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,12 +6,15 @@ const { getProducts,
     getSingleProduct,
     updateProduct,
     deleteProduct,
-    createProductReview } = require('../controllers/productController')
+    createProductReview,
+    getProductReviews } = require('../controllers/productController')
 
 router.route('/products').get(getProducts)
 router.route('/admin/product/new').post(isAuthenticated, authorizeRoles('admin'), newProduct)
 router.route('/product/:id').get(getSingleProduct)
 router.route('/admin/product/:id').put(isAuthenticated, authorizeRoles('admin'), updateProduct)
     .delete(isAuthenticated, authorizeRoles('admin'), deleteProduct)
+
 router.route('/review').put(isAuthenticated, createProductReview)
+router.route('/reviews').get(isAuthenticated, getProductReviews)
 module.exports = router
