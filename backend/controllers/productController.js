4cfe93bef2720844235b904293f969d11686185a
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-06-28 09:36:49 +0700

    Fetching Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,20 +21,22 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
 
-    const resPerPage = 4;
-    const productCount = await Product.countDocuments();
+    const resPerPage = 8;
+    const productsCount = await Product.countDocuments();
     const apiFeatures = new APIFeatures(Product.find(), req.query)
         .search()
         .filter()
         .pagination(resPerPage)
     const products = await apiFeatures.query;
 
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        productCount,
-        products
-    })
+    setTimeout(() => {
+        res.status(200).json({
+            success: true,
+            productsCount,
+            products
+        })
+    }, 1000)
+
 })
 
 //Get single product details => /api/v1/admin/product/:id
