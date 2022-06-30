e164d3bd32c6c565458d59f5f0dce7e5b3e11b89
hoangtuanday123 <pnhtuanhcmus@gmail.com>
2022-06-30 18:21:05 +0700

    Login form và handle Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from './components/product/ProductDetails';
+import Login from './components/user/Login'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
+          <Route path="/login" component={Login} />
         </div>
         <Footer />
       </div>
