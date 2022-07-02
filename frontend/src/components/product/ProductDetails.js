1917b8f5a9969a6f2adea9a0fdc637f26621c8fb
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-07-02 07:47:46 +0700

    Xử lý tăng giảm số lượng khi thêm sách vào giỏ hàng

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Carousel } from 'react-bootstrap'
 import Loader from '../layout/Loader'
 import MetaData from '../layout/MetaData'
@@ -9,8 +9,10 @@ import { formatMoney } from './Product'
 
 const ProductDetails = ({ match }) => {
 
+    const [quantity, setQuantity] = useState(1)
     const dispatch = useDispatch()
     const alert = useAlert()
+
     const { loading, error, product } = useSelector(state => state.productDetails)
 
     useEffect(() => {
@@ -21,6 +23,25 @@ const ProductDetails = ({ match }) => {
             dispatch(clearErrors())
         }
     }, [dispatch, alert, error, match.params.id])
+
+    const increaseQty = () => {
+        const count = document.querySelector('.count')
+        if (count.valueAsNumber >= product.stock) {
+            return
+        }
+        const qty = count.valueAsNumber + 1
+        setQuantity(qty)
+    }
+
+    const decreaseQty = () => {
+        const count = document.querySelector('.count')
+        if (count.valueAsNumber <= 1) {
+            return
+        }
+        const qty = count.valueAsNumber - 1
+        setQuantity(qty)
+    }
+
     return (
         <Fragment>
             {loading ? <Loader /> : (
@@ -53,11 +74,11 @@ const ProductDetails = ({ match }) => {
 
                             <p id="product_price">{formatMoney(product.price)}₫</p>
                             <div className="stockCounter d-inline">
-                                <span className="btn btn-danger minus">-</span>
+                                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
-                                <input type="number" className="form-control count d-inline" value="1" readOnly />
+                                <input type="number" className="form-control count d-inline" value={quantity} readOnly />
 
-                                <span className="btn btn-primary plus">+</span>
+                                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                             </div>
                             <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Thêm vào giỏ hàng</button>
 
