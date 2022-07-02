e2f4c40b9bd03b564b06073187f44fc7d8b4986e
giatin091202 <20127645@student.hcmus.edu.vn>
2022-07-02 09:03:29 +0700

    Thiết kế giao diện quản lý giỏ hàng

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from './components/product/ProductDetails';
+import Cart from './components/cart/Cart';
 import Login from './components/user/Login'
 import Register from './components/user/Register'
 import { loadUser } from './actions/userActions'
@@ -14,6 +15,7 @@ import { UpdateProfile } from './components/user/UpdateProfile';
 import { UpdatePassword } from './components/user/UpdatePassword'
 import { ForgotPassword } from './components/user/ForgotPassword'
 import { NewPassword } from './components/user/NewPassword'
+
 
 function App() {
 
@@ -28,6 +30,7 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
+          <Route path="/cart" component={Cart} exact />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/password/forgot" component={ForgotPassword} exact />
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
     const alert = useAlert()
     const dispatch = useDispatch()
     const { user, loading } = useSelector(state => state.auth)
+    const { cartItems } = useSelector(state => state.cart)
     const logoutHandler = () => {
         dispatch(logout())
         alert.success('Đăng xuất thành công.')
@@ -33,7 +34,7 @@ const Header = () => {
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                     <Link to="/cart" style={{ textDecoration: 'none' }}>
                         <span id="cart" className="ml-3">Giỏ hàng</span>
-                        <span className="ml-1" id="cart_count">2</span>
+                        <span className="ml-1" id="cart_count">{cartItems.length}</span>
                     </Link>
                     {user ? (
                         <div className='="ml-4 dropdown d-inline'>
