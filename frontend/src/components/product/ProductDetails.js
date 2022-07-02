7bcefc55cd9db4b6f6ad00bbd658af53b8f2979c
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-07-02 08:26:44 +0700

    Xử lý button thêm vào giỏ hàng

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -6,6 +6,7 @@ import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductDetails, clearErrors } from '../../actions/productActions'
 import { formatMoney } from './Product'
+import { addItemToCart } from '../../actions/cartActions'
 
 const ProductDetails = ({ match }) => {
 
@@ -24,6 +25,10 @@ const ProductDetails = ({ match }) => {
         }
     }, [dispatch, alert, error, match.params.id])
 
+    const addToCart = () => {
+        dispatch(addItemToCart(match.params.id, quantity))
+        alert.success('Sách đã được thêm vào giỏ hàng')
+    }
     const increaseQty = () => {
         const count = document.querySelector('.count')
         if (count.valueAsNumber >= product.stock) {
@@ -80,7 +85,8 @@ const ProductDetails = ({ match }) => {
 
                                 <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                             </div>
-                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Thêm vào giỏ hàng</button>
+                            <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4"
+                                disabled={product.stock === 0} onClick={addToCart}>Thêm vào giỏ hàng</button>
 
                             <hr />
 
