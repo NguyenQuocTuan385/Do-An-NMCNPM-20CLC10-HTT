cd5324f052ed8222d97f0f99215df468b63aa4b5
giatin091202 <20127645@student.hcmus.edu.vn>
2022-07-02 10:04:23 +0700

    Tính tổng giá trị đơn hàng và số lượng

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -86,13 +86,15 @@ const Cart = () => {
 
                         <div className="col-12 col-lg-3 my-4">
                             <div id="order_summary">
-                                <h4>Order Summary</h4>
+                                <h4>Tổng đơn hàng</h4>
                                 <hr />
-                                <p>Subtotal:  <span className="order-summary-values">3 (Units)</span></p>
-                                <p>Est. total: <span className="order-summary-values">$765.56</span></p>
+                                <p>Số lượng hàng:  <span className="order-summary-values">
+                                    {cartItems.reduce((acc, item) => (acc + Number(item.quantity)), 0)}</span></p>
+                                <p>Tổng số tiền: <span className="order-summary-values">
+                                    {cartItems.reduce((acc, item) => formatMoney(acc + item.quantity * item.price), 0)}₫</span></p>
 
                                 <hr />
-                                <button id="checkout_btn" className="btn btn-primary btn-block">Check out</button>
+                                <button id="checkout_btn" className="btn btn-primary btn-block">Thanh toán</button>
                             </div>
                         </div>
                     </div>
