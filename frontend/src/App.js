7413953c5c4b31b48d22a307e7005bb9ff7cd75e
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-07-03 08:57:56 +0700

    Thiết kế Dashboard Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,38 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from './components/product/ProductDetails';
+
+//Cart imports
+import Shipping from './components/cart/Shipping';
+import ConfirmOrder from './components/cart/ConfirmOrder';
+import Payment from './components/cart/Payment';
+import OrderSuccess from './components/cart/OrderSuccess';
 import Cart from './components/cart/Cart';
-import Login from './components/user/Login'
-import Register from './components/user/Register'
-import { loadUser } from './actions/userActions'
-import store from './store'
-import Profile from './components/user/Profile';
+
+//Order Imports
+import ListOrders from './components/order/ListOrders';
+import OrderDetails from './components/order/OrderDetails';
+
+//Auth or users imports
 import { ProtectedRoute } from './components/route/ProtectedRoute';
 import { UpdateProfile } from './components/user/UpdateProfile';
 import { UpdatePassword } from './components/user/UpdatePassword'
 import { ForgotPassword } from './components/user/ForgotPassword'
 import { NewPassword } from './components/user/NewPassword'
-import Shipping from './components/cart/Shipping';
-import ConfirmOrder from './components/cart/ConfirmOrder';
+import Login from './components/user/Login'
+import Register from './components/user/Register'
+import Profile from './components/user/Profile';
+import { loadUser } from './actions/userActions'
+import store from './store'
 import axios from 'axios';
-import Payment from './components/cart/Payment';
-import OrderSuccess from './components/cart/OrderSuccess';
-import ListOrders from './components/order/ListOrders';
-import OrderDetails from './components/order/OrderDetails';
 
 //Payment
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+
+//Admin imports
+import Dashboard from './components/admin/Dashboard';
+
 function App() {
   const [stripeApiKey, setStripeApiKey] = useState('')
 
@@ -67,7 +77,10 @@ function App() {
           <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
           <ProtectedRoute path="/orders/me" component={ListOrders} exact />
           <ProtectedRoute path="/order/:id" component={OrderDetails} exact />
+
+
         </div>
+        <ProtectedRoute path="/dashboard" isAdmin={true} component={Dashboard} exact />
         <Footer />
       </div>
     </Router>
